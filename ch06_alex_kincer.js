f77750eadeca0bff8df5261231b24a77bfb88776
alex <rakincer@georgetowncollege.edu>
2025-09-30 22:38:31 -0400

    Chap 6 HW

--- ch06_alex_kincer.js
+++ ch06_alex_kincer.js
@@ -55,13 +55,47 @@ class Group {
         if (!this.members.includes(x)) {
             this.members.push(x)
         }
-        // if x is one of the members, do nothing
-        // if not, push x onto the members
+    }
+    has(x) {
+        return this.members.includes(x);
+    }
+    delete(x) {
+        this.members = this.members.filter(v => v !== x);
+    }
+    static from(collection) {
+        let group = new Group();
+        for (let value of collection) {
+            group.add(value);
+        }
+        return group;
     }
 }
 
-let myGroup = new Group;
-console.log(myGroup);
-myGroup.add(3);
-console.log(myGroup);
+/*using the book examples*/
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
 
+/*
+I recieved the first half of this code from in class with Dr. White. I cannot remember the exact
+date because I did not write it down for some reason, but it was last week. Adding onto the code
+I was shown in class, I used "has" because the text explained that it could be used as a way to 
+test whether a value is a member of the group, which was necessary for this problem. The "has" 
+command returns true or false depending on whether the value is in the group or not. Without "has",
+we wouldn't know if the number/x given was in there. I used "delete" because the question said the
+code needed to be able to remove a value from the room, according to the text, "delete" does that
+to prevent the group from continuing to grow and nothing ever being removed. Finally, I used "static"
+because the question asked for a static method called "from." Static method is used to call on a
+class itself rather than only a specific group. The want for "from" is so a new group can be easily
+created from an array or iterable. All of these were necessary for this exercsise and seemed to have
+worked well. The only bit of confusion I ran into was the use of static/from so, I had a ChatGPT 
+conversation to understand it a little bit better. This conversation aided in making my code and
+utilizing the ideas of the other things such as "has" and "delete". Here is this conversation:
+https://chatgpt.com/share/68dc9140-5684-8002-9758-2c2d63f0b308
+*/
