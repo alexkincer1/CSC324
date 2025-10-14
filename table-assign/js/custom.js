fdd862445424edb9c0c25e122f37873dffd0ed0e
alex <rakincer@georgetowncollege.edu>
2025-10-14 13:32:45 -0400

    Table Assign updated

--- table-assign/js/custom.js
+++ table-assign/js/custom.js
@@ -35,72 +35,39 @@
   }
 ];
 
-const target = document.getElementById("bhangra");
-const bhangraTable = makeTable(artists);
-target.appendChild(bhangraTable);
+// locate the table:
+const tab = document.querySelector("#bhangra");
 
-/* The place of code above looks at each element in mountains before appending it to the
-newly created table. */
+ // declare a string to hold the inner html.
 
-// utility function
+let contents = "<tbody>";
 
-// function to make a tabke from an array of objects:
-function makeTable(arr) {
-  // create a table element:
-  const table = document.createElement("table");
-  // use first object inthe array to make the header:
-  table.innerHTML = makeTableHeader(arr[0]);
-  for (let obj of arr) {
-    table.innerHTML += makeTableRow(obj);
-  }
-  console.log(table.innerHTML);
-  // do not forget to return to the table:
-  return table
-}
+// get in the header row:
+contents += `
+    <tr>
+      <th>Name</th>
+      <th>Year of Birth</th>
+      <th>Link</th>
+    </tr>
+    `;
 
-// function to make a table header:
-function makeTableHeader(obj) {
-  let inner = "<tr>";
-  // fill it in with the th-elements:
-  for (let columnName of Object.keys(obj)) {
-    if (columnName !== "url") {
-      inner += `<th>${columnName}</th>`;
-    }
-  }
-  // add the closer:
-  inner += "</tr>";
-  console.log(inner);
-  return inner;
-}
+// now loop to make the data-rows:
 
-// function to make a table row:
-function makeTableRow(obj) {
-  let inner = "<tr>";
-  for (let key of Object.keys(obj)) {
-    if (key === "link") {
-      // creating the clickable link column
-      const anchorHTML = `<a href="${obj[key]}" target="_blank">${obj[key]}</a>`;
-      inner += `<td>${anchorHTML}</td>`;
-      } else { 
-        inner += `<td>${obj[key]}</td>`;
-      }
-    }
-    inner += "</tr>";
-    return inner;
-  }
+artists.forEach(artist => {
+  // open the row:
+  contents += "<tr>";
+  contents += `<td>${artist.name}</td>`;
+  contents += `<td>${artist.birthYear}</td>`;
+  contents += `<td><a href="${artist.link}" target = "_blank">${artist.link}</a></td>`;
+  // close the row:
+  contents += "</tr>"
+});
 
+// close out the table body:
+contents += "</tbody>";
 
-/* I recieved majority of this answer through in-class discussion and a screenshare
-from classmate Mason on October 7, 2025. */
-
-/*
-Taking Mason's code from the screenshare in class, I took the makeTableRow portion
-and changed "name" in line 82 and changed it to key so the hyperlink on the actual
-browser appears in the link column to take you to youtube. I also deleted 3-4 if else
-rows in his code as Dr. White stated it was unnecessary and it was causing a mix up for
-me in what I was trying to link and not link. His original code gave no links, then
-as I was editing I played commands in the wrong spot so it was linking name rather
-than the actual link column. By moving this, there is now a clickable link. I changed
-1 line (line 82) and omitted a few lines from his code but all were in the makeTableRow
-section.
-*/
+// now make contents be the inner html of
+// the table:
+tab.innerHTML = contents;
+ 
+ 
